8c6ac952cad50a5fec62c41af360d15976cd4b3a
mlin1026 <mike911026@gmail.com>
2022-02-01 14:32:04 -0800

    Tester and testing java file

---
+++ SkillTesting.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import  org.junit.*;
+
+public class SkillTesting {
+    @Test
+    public void testing(){
+        assertEquals(3, SkillDemostra.multiply(1, 3));
+    }
+}
